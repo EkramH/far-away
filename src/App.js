9656b46c142761b22dedbe77cc49229dce25731d
Ekram Hosen <ekramhosen329@gmail.com>
2024-01-05 20:04:38 +0600

    style component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
@@ -15,19 +20,19 @@ function Logo() {
 
 function Form() {
   return (
-    <div>
+    <div className="add-form">
       <h3>What do you need for your trip?</h3>
     </div>
   );
 }
 
 function PackingList() {
-  return <div>List</div>;
+  return <div className="list">List</div>;
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your list, and you already packet X (X%).</em>
     </footer>
   );
