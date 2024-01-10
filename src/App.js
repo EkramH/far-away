3a914ce07370b0140c7fff3172e5cd923d1b84fb
Ekram Hosen <ekramhosen329@gmail.com>
2024-01-10 22:04:34 +0600

    lifting up state

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form addItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,10 +27,9 @@ function Logo() {
   return <h1>Far Away</h1>;
 }
 
-function Form() {
+function Form({ addItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
-  console.log(quantity);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -32,6 +37,8 @@ function Form() {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    addItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -63,11 +70,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item key={item.id} item={item} />
         ))}
       </ul>
